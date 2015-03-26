bf200abbcd47aa306a679daabd5f23216a63f066
Alexander Dupree <truedupree@gmail.com>
2015-03-26 15:11:57 +0300

    new routong system on fronend test

--- apps/frontend/modules/main/actions/actions.class.php
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -36,4 +36,97 @@ class mainActions extends sfActions
       $this->content = $sectionContent;
       $this->html = $sectionContent->getHtml();
   }
+
+    public function executeSection(sfWebRequest $request)
+    {
+        $slug = $request->getParameter("slug");
+        $subSlug = $request->getParameter("sub_slug");
+        $thirdLevelSlug = $request->getParameter("sub_sub_slug");
+
+        $this->content = null;
+
+        if($thirdLevelSlug)
+        {
+            //It means that this is childless section e.g. Medicine article or News Article
+            $this->content = $this->getThirdLevelSectionContentBySlugs($thirdLevelSlug);
+        }
+        else
+        {
+            //Here we should check for subSlug to determine the level of this section
+            if($subSlug)
+            {
+                //It means that we're in second level section
+                $this->content = $this->getSecondLevelSectionContentBySlugs($subSlug);
+            }
+            else if($slug)
+            {
+                //That means that we're in one of first-level sections
+                $this->content = $this->getFirstLevelSectionContentBySlug($slug);
+            }
+            else
+            {
+                //if there is no slugs in this url, probably it is the homepage or some mistake
+                //anyway we should force user to the homepage
+
+                $this->redirect('main/index');
+            }
+        }
+    }
+
+    /**
+     * This method grabs content for specified first-level section
+     *
+     * @return Content
+     */
+    private function getFirstLevelSectionContentBySlug($slug)
+    {
+        $section = SectionTable::getInstance()->findOneBy("slug", $slug);
+        if(count($section)>0) {
+            return ContentTable::getInstance()->findOneBy("section_id", $section->getId());
+        }
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * This method grabs content for specified second-level section
+     *
+     * @return Content
+     */
+    private function getSecondLevelSectionContentBySlugs($subSlug, $slug=null)
+    {
+        //We don't need $slug in this part, it have been added just to make code more flexible
+        //Maybe client would ask you to make some bread crumbs, and you would like to return not just Content object,
+        //but maybe an array with links to previous sections and content of current section
+
+        $section = SectionTable::getInstance()->findOneBy("slug", $subSlug);
+        if(count($section)>0) {
+            return ContentTable::getInstance()->findOneBy("section_id", $section->getId());
+        }
+        else{
+            return null;
+        }
+
+    }
+
+    /**
+     * This method grabs content for specified third-level section
+     *
+     * @return Content
+     */
+    private function getThirdLevelSectionContentBySlugs($subSubSlug, $slug=null, $subSlug=null)
+    {
+        //We don't need $slug in this part, it have been added just to make code more flexible
+        //Maybe client would ask you to make some bread crumbs, and you would like to return not just Content object,
+        //but maybe an array with links to previous sections and content of current section
+
+        $section = SectionTable::getInstance()->findOneBy("slug", $subSubSlug);
+        if(count($section)>0) {
+            return ContentTable::getInstance()->findOneBy("section_id", $section->getId());
+        }
+        else{
+            return null;
+        }
+    }
 }
